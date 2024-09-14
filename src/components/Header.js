dbf49df69b103ac4fd593708b163d8ca611e5ed4
Akash Ur Rehman <akashurrehman456@gmail.com>
2024-09-14 05:13:25 +0500

    feat:fix header responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,13 @@ const Header = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  // Function to handle closing the menu
+  const handleLinkClick = () => {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+    }
+  };
+
   return (
     <header>
       <div className="container">
@@ -20,16 +27,16 @@ const Header = () => {
             <span className="bar"></span>
           </button>
           <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
-            <li><Link to="/">{translations.home}</Link></li>
-            <li><Link to="/about-us">{translations.aboutUs}</Link></li>
-            <li><Link to="/our-ministries">{translations.ourMinistries}</Link></li>
-            <li><Link to="/upcoming-courses">{translations.upcomingCourses}</Link></li>
-            <li><Link to="/get-involved">{translations.getInvolved}</Link></li>
-            <li><Link to="/prayer-points">{translations.prayerPoints}</Link></li>
-            <li><Link to="/donate">{translations.donate}</Link></li>
-            <li><Link to="/events">{translations.events}</Link></li>
-            <li><Link to="/blog-news">{translations.blogNews}</Link></li>
-            <li><Link to="/contact-us">{translations.contactUs}</Link></li>
+            <li><Link to="/" onClick={handleLinkClick}>{translations.home}</Link></li>
+            <li><Link to="/about-us" onClick={handleLinkClick}>{translations.aboutUs}</Link></li>
+            <li><Link to="/our-ministries" onClick={handleLinkClick}>{translations.ourMinistries}</Link></li>
+            <li><Link to="/upcoming-courses" onClick={handleLinkClick}>{translations.upcomingCourses}</Link></li>
+            <li><Link to="/get-involved" onClick={handleLinkClick}>{translations.getInvolved}</Link></li>
+            <li><Link to="/prayer-points" onClick={handleLinkClick}>{translations.prayerPoints}</Link></li>
+            <li><Link to="/donate" onClick={handleLinkClick}>{translations.donate}</Link></li>
+            <li><Link to="/events" onClick={handleLinkClick}>{translations.events}</Link></li>
+            <li><Link to="/blog-news" onClick={handleLinkClick}>{translations.blogNews}</Link></li>
+            <li><Link to="/contact-us" onClick={handleLinkClick}>{translations.contactUs}</Link></li>
           </ul>
           <button className="language-toggle" onClick={toggleLanguage}>
             {language === 'EN' ? 'සිංහල' : 'English'}
